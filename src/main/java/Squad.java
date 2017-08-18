add903aac11c4a951d68b572391f7753ec8e238a
charliekaks <kaksunchained@gmail.com>
2017-08-18 09:15:27 +0300

    implemented tests for Squad class

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -2,5 +2,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Squad{
-    
+    private String squadName;
+    private int mId;
+    private static List<Squad> herosSquad = new ArrayList<Squad>();
+    private List<Heros> mHeros;
+
+    public Squad(String name){
+        squadName = name;
+        herosSquad.add(this);
+    }
+    public String getName(){
+        return squadName;
+    }
+    public static List<Squad>all(){
+        return herosSquad;
+    }
+    public static void clear(){
+        herosSquad.clear();
+    }
 }
--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,5 +1,32 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 public class SquadTest{
-    
+  @Test
+  public void squad_instanciatesTheObjectAppropriately_true(){
+      Squad heroSquad = new Squad("Young Justice");
+      assertEquals(true, heroSquad instanceof Squad);
+  }
+  @Test
+  public void getName_instanciatesTheNameCorrectly_string(){
+    Squad heroSquad = new Squad("Young Justice");
+    assertEquals("Young Justice", heroSquad.getName());    
+  }
+  @Test
+  public void all_returnAllInstancesOfSquad_true(){
+    Squad firstHeroSquad = new Squad("Young Justice");
+    Squad secondHeroSquad2 = new Squad("League of Legends"); 
+    assertEquals(true, Squad.all().contains(firstHeroSquad));
+    assertEquals(true, Squad.all().contains(secondHeroSquad2));
+  }
+  @Test
+  public void clear_emptiesAllCategoriesFromList_0(){
+    Squad heroSquad = new Squad("Young Justice");
+    Squad.clear();
+    assertEquals(Squad.all().size(), 0);
+  }
+  @Test
+  public void getId_SquadINstanceWithId_1(){
+      
+  }
+
 }
